fca9582b4e372c0542edda26eaa03d852e4c9991
roblebob <robert.koppisch@gmail.com>
2024-04-19 21:05:01 +0200

    Iteration 3 | Add Links - 4. In the HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import StudentCard from "../components/StudentCard";
 
 import studentsData from "../assets/students.json";
@@ -9,19 +10,26 @@ function HomePage() {
 
   return (
     <div className="border-2 border-rose-500 m-2">
-        <h1>Home Page</h1>
-        <div className="flex justify-between items-center p-2 font-bold border-b w-full">
-          <span className="flex items-center justify-center" style={{ flexBasis: "20%" }}>Image</span>
-          <span style={{ flexBasis: "20%" }}>Name</span>
-          <span style={{ flexBasis: "20%" }}>Program</span>
-          <span style={{ flexBasis: "20%" }}>Email</span>
-          <span style={{ flexBasis: "20%" }}>Phone</span>
-        </div>
+      <h1>Home Page</h1>
+      <div className="flex justify-between items-center p-2 font-bold border-b w-full">
+        <span
+          className="flex items-center justify-center"
+          style={{ flexBasis: "20%" }}
+        >
+          Image
+        </span>
+        <span style={{ flexBasis: "20%" }}>Name</span>
+        <span style={{ flexBasis: "20%" }}>Program</span>
+        <span style={{ flexBasis: "20%" }}>Email</span>
+        <span style={{ flexBasis: "20%" }}>Phone</span>
+      </div>
 
       {students &&
         students.map((student) => {
           return (
+            <Link to={`/students/${student._id}`} key={`/students/${student._id}`}>
               <StudentCard key={student._id} {...student} />
+            </Link>
           );
         })}
     </div>
