b7dbc1ccf7ab8eb3031500337709248699375e5b
roblebob <robert.koppisch@gmail.com>
2024-04-19 20:52:58 +0200

    Iteration 3 | Add Links - 3. In the StudentDetailsPage component

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -71,9 +71,11 @@ function StudentDetailsPage() {
             </div>
 
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-              Back
-            </button>
+            <Link to="/">
+              <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+                Back
+              </button>
+            </Link>
           </>
         )}
       </div>
